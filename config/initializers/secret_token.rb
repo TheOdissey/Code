b6b5af37de9b2b8dc31638cc17b47f0dd4f15286
saasbook <saasbook@saasbook.(none)>
2013-10-17 14:10:07 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spillio::Application.config.secret_token = 'f8c04ab4be9e420c1542a64b5a79e07f6f51b03c61fa44e8cd8907b80475f76133bfc352a82677d749da8e4bc989ef0a004b4474e884280cc15e26bc4c6e40a1'
